3b9d18a3f2556b6bbe9a4541dc100adad4eba9af
LouisMelo <jsyzliuxb0918@gmail.com>
2021-03-22 22:26:53 +0800

    feat: collapse creator by default

--- src/components/transactions/TransactionCreator.jsx
+++ src/components/transactions/TransactionCreator.jsx
@@ -9,6 +9,7 @@ import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Typography from '@material-ui/core/Typography';
+import Collapse from '@material-ui/core/Collapse'
 import { makeStyles } from '@material-ui/core/styles';
 
 import { addTransaction } from '../../store/actions/transactionActions'
@@ -24,12 +25,14 @@ const useStyles = makeStyles((theme) => ({
       marginRight: 'auto',
     },
   },
+  collapseButton: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+  },
   paper: {
-    marginTop: theme.spacing(3),
     marginBottom: theme.spacing(3),
     padding: theme.spacing(2),
     [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-      marginTop: theme.spacing(2),
       marginBottom: theme.spacing(2),
       padding: theme.spacing(3),
     },
@@ -51,6 +54,8 @@ const useStyles = makeStyles((theme) => ({
 const TransactionCreator = () => {
   const classes = useStyles();
   const dispatch = useDispatch()
+
+  const [showCreator, setShowCreator] = useState(false)
 
   const [type, setType] = useState('b');
   const [code, setCode] = useState('');
@@ -77,6 +82,10 @@ const TransactionCreator = () => {
     const { value } = event.target
 
     setAmount(value)
+  }
+
+  const handleShowCreatorMenu = () => {
+    setShowCreator(!showCreator)
   }
 
   useEffect(() => {
@@ -116,72 +125,85 @@ const TransactionCreator = () => {
   }
 
   return (
-    <Paper className={classes.paper}>
-      <form className={classes.form} noValidate>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={4}>
-            <FormControl className={classes.formControl}>
-              <InputLabel id="transaction-type-select-label">选择买入/卖出</InputLabel>
-              <Select
-                labelId="transaction-type-select-label"
-                id="transaction-type-select"
-                value={type}
-                onChange={handleChangeType}
+    <>
+      <Button
+        fullWidth
+        variant="contained"
+        color="primary"
+        className={classes.collapseButton}
+        onClick={handleShowCreatorMenu}
+      >
+        { showCreator ? '收起' : '添加记录' }
+      </Button>
+      <Collapse in={showCreator}>
+        <Paper className={classes.paper}>
+          <form className={classes.form} noValidate>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={4}>
+                <FormControl className={classes.formControl}>
+                  <InputLabel id="transaction-type-select-label">选择买入/卖出</InputLabel>
+                  <Select
+                    labelId="transaction-type-select-label"
+                    id="transaction-type-select"
+                    value={type}
+                    onChange={handleChangeType}
+                  >
+                    <MenuItem value={'b'}>买入</MenuItem>
+                    <MenuItem value={'s'}>卖出</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={8}>
+                <TextField
+                  required
+                  fullWidth
+                  id="code"
+                  label="股票代码"
+                  name="code"
+                  value={code}
+                  onChange={handleChangeCode}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="price"
+                  label="价格"
+                  id="price"
+                  value={price}
+                  onChange={handleChangePrice}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="amount"
+                  label="数量"
+                  id="amount"
+                  value={amount}
+                  onChange={handleChangeAmount}
+                />
+              </Grid>
+            </Grid>
+            <div className={classes.footer}>
+              <Typography variant='subtitle2'>
+                { `此次**${type === 'b' ? '买入' : '卖出'}**总金额为：¥${total}`}
+              </Typography>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                onClick={handleSubmit}
               >
-                <MenuItem value={'b'}>买入</MenuItem>
-                <MenuItem value={'s'}>卖出</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} md={8}>
-            <TextField
-              required
-              fullWidth
-              id="code"
-              label="股票代码"
-              name="code"
-              value={code}
-              onChange={handleChangeCode}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              required
-              fullWidth
-              name="price"
-              label="价格"
-              id="price"
-              value={price}
-              onChange={handleChangePrice}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              required
-              fullWidth
-              name="amount"
-              label="数量"
-              id="amount"
-              value={amount}
-              onChange={handleChangeAmount}
-            />
-          </Grid>
-        </Grid>
-        <div className={classes.footer}>
-          <Typography variant='subtitle2'>
-            { `此次**${type === 'b' ? '买入' : '卖出'}**总金额为：¥${total}`}
-          </Typography>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            onClick={handleSubmit}
-          >
-            确认
-          </Button>
-        </div>
-      </form>
-    </Paper>
+                确认
+              </Button>
+            </div>
+          </form>
+        </Paper>
+      </Collapse>
+    </>
   );
 }
 
